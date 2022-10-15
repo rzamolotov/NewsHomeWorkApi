195d4f29c6ad7fc24203ae0a42e6b71ef225beed
Roman Zamolotov <rzamolotov@gmail.com>
2022-10-15 12:33:54 +0300

    Initial Commit

---
+++ NewsHomeWorkApi/NewsHomeWorkApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsHomeWorkApiApp.swift
+//  NewsHomeWorkApi
+//
+//  Created by Роман Замолотов on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsHomeWorkApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
